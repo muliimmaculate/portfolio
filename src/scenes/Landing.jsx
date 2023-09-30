f1178aca7edd299844518c2342db142f4d6df9ab
immaculate muli <muliimaculate@gmail.com>
2023-09-30 15:01:47 +0300

    another commit

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -8,23 +8,15 @@ import React from 'react';
 const Landing = () => {
   return (
     <section className="h-full max-h-[640px] mb- xl:mb-24">
-      <div className="flex flex-col lg:flex-row">
+      <div className="flex justify-between items-center">
         {/* IMAGE (Left side) */}
         <div className="lg:mr-8 xl:mr-[135px]">
-          <img src={ProfileImage} alt='' className="w-150 h-120 rounded-full" />
+          <img src={ProfileImage} alt='' className="w-150 h-120 rounded-full
+           transition duration-500 z-10 max-w-[400px] saturate-200" />
         </div>
         {/* CONTENT (Right side) */}
-        <div className="text-center lg:text-left px-4 lg:px-0">
+        <div>
           
-          <h1 className="text-4xl lg:text-[58px] font-semibold leading-none mb-6">
-            Hello, I am <span>IMM</span>
-          </h1>
-          <p className="max-w-[480px] mb-8 ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, 
-          ratione repudiandae vel reprehenderit architecto voluptatum at,
-          repellendus illo repellat autem ad! Assumenda magnam ut beatae error,
-          dicta aliquam atque facilis.
-          </p>
         </div>
       </div>
     </section>
